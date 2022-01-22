b9952b185ca686e44ac864de01f59b800b8d26cc
Zero <58396888+Zer0-XD@users.noreply.github.com>
2022-01-22 22:56:40 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Header from '../Components/Header'
-import { motion } from 'framer-motion';
-
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      {/* <Header /> */}
-      <motion.div initial="pageInitial" animate="pageAnimate" variants={{
-        pageInitial: {
-          opacity: 0
-        },
-        pageAnimate: {
-          opacity: 1
-        },
-      }}>
-        <Component {...pageProps} />
-      </motion.div>
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
